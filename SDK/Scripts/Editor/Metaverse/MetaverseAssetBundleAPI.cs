d6e8c157f9bb2e40563a83a38dbf46d93fbe94c3
Soloman N <sxnorthrop15@gmail.com>
2024-04-09 08:58:02 -0400

    Update MetaverseAssetBundleAPI.cs

--- SDK/Scripts/Editor/Metaverse/MetaverseAssetBundleAPI.cs
+++ SDK/Scripts/Editor/Metaverse/MetaverseAssetBundleAPI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.Build;
