1639877b33f512d8a49ce8130f662c2911a04c0e
Soloman N <sxnorthrop15@gmail.com>
2024-09-23 08:07:05 -0400

    Update MetaOVRProjectSetupRenderingTasksFix.cs

--- SDK/Editor/BugFixes/MetaOVRProjectSetupRenderingTasksFix.cs
+++ SDK/Editor/BugFixes/MetaOVRProjectSetupRenderingTasksFix.cs
@@ -1,4 +1,3 @@
-using UnityEngine;
 using UnityEditor;
 using System.IO;
 
@@ -12,14 +11,13 @@ namespace MetaverseCloudEngine.Unity.Editors.BugFixes
         {
             var path = "Library/PackageCache/com.meta.xr.sdk.core@68.0.2/Editor/OVRProjectSetup/Tasks/Implementations/OVRProjectSetupRenderingTasks.cs";
             if (!File.Exists(path)) return;
-            var text = System.IO.File.ReadAllText(path);
+            var text = File.ReadAllText(path);
             var brokenLine = "?.Any(cameraData => cameraData.cameraStack?.Any() ?? false) ?? false;";
             if (text.Contains(brokenLine)) {
                 var newText = text.Replace(
                     brokenLine,
                     "?.Any(cameraData => cameraData != null && cameraData.scriptableRenderer != null && (cameraData.cameraStack?.Any() ?? false)) ?? false;");
-                System.IO.File.WriteAllText(path, newText);
-                // Recompile
+                File.WriteAllText(path, newText);
                 UnityEditor.Compilation.CompilationPipeline.RequestScriptCompilation();
             }
         }
