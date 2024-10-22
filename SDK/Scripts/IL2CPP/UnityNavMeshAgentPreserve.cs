e140b89b4ef5efbb9ec328c7468f3fc209387098
Soloman N <sxnorthrop15@gmail.com>
2024-10-22 14:09:06 -0400

    Update UnityNavMeshAgentPreserve.cs

--- SDK/Scripts/IL2CPP/UnityNavMeshAgentPreserve.cs
+++ SDK/Scripts/IL2CPP/UnityNavMeshAgentPreserve.cs
@@ -1,46 +1,52 @@
-using System;
+#if MV_UNITY_AI_NAV
+
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Scripting;
 
-[Preserve]
-public class UnityNavMeshAgentPreserve : MonoBehaviour
+namespace MetaverseCloudEngine.Unity.Internal.IL2CPP
 {
-    void Start()
+    [Preserve]
+    internal class UnityNavMeshAgentPreserve : MonoBehaviour
     {
-        try
+        void Start()
         {
-            NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
-            navMeshAgent.destination = new Vector3(0, 0, 0);
-            navMeshAgent.SetDestination(Vector3.zero);
-            navMeshAgent.velocity = Vector3.zero;
-            navMeshAgent.angularSpeed = 0;
-            navMeshAgent.acceleration = 0;
-            navMeshAgent.autoTraverseOffMeshLink = false;
-            navMeshAgent.autoRepath = false;
-            navMeshAgent.autoBraking = false;
-            navMeshAgent.height = 0;
-            navMeshAgent.baseOffset = 0;
-            navMeshAgent.radius = 0;
-            navMeshAgent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance;
-            navMeshAgent.avoidancePriority = 0;
-            navMeshAgent.stoppingDistance = 0;
-            var r = navMeshAgent.remainingDistance;
-            var p = navMeshAgent.path;
-            var c = navMeshAgent.pathStatus;
-            var s = navMeshAgent.isStopped;
-            var w = navMeshAgent.isOnNavMesh;
-            var a = navMeshAgent.hasPath;
-            var b = navMeshAgent.pathPending;
-            var d = navMeshAgent.updatePosition;
-            var e = navMeshAgent.updateRotation;
-            var f = navMeshAgent.nextPosition;
-            var g = navMeshAgent.agentTypeID;
-            var v = navMeshAgent.velocity;
-            var steering = navMeshAgent.steeringTarget;
-        }
-        catch (Exception exception)
-        {
+            try
+            {
+                NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+                navMeshAgent.destination = new Vector3(0, 0, 0);
+                navMeshAgent.SetDestination(Vector3.zero);
+                navMeshAgent.velocity = Vector3.zero;
+                navMeshAgent.angularSpeed = 0;
+                navMeshAgent.acceleration = 0;
+                navMeshAgent.autoTraverseOffMeshLink = false;
+                navMeshAgent.autoRepath = false;
+                navMeshAgent.autoBraking = false;
+                navMeshAgent.height = 0;
+                navMeshAgent.baseOffset = 0;
+                navMeshAgent.radius = 0;
+                navMeshAgent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance;
+                navMeshAgent.avoidancePriority = 0;
+                navMeshAgent.stoppingDistance = 0;
+                var r = navMeshAgent.remainingDistance;
+                var p = navMeshAgent.path;
+                var c = navMeshAgent.pathStatus;
+                var s = navMeshAgent.isStopped;
+                var w = navMeshAgent.isOnNavMesh;
+                var a = navMeshAgent.hasPath;
+                var b = navMeshAgent.pathPending;
+                var d = navMeshAgent.updatePosition;
+                var e = navMeshAgent.updateRotation;
+                var f = navMeshAgent.nextPosition;
+                var g = navMeshAgent.agentTypeID;
+                var v = navMeshAgent.velocity;
+                var steering = navMeshAgent.steeringTarget;
+            }
+            catch
+            {
+                /* ignored */
+            }
         }
     }
 }
+#endif
