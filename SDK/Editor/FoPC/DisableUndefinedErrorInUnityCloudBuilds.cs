5cc693d205ad00e3694cac30c9693c64626769fb
Soloman <sxnorthrop15@gmail.com>
2024-12-01 13:08:13 -0500

    Create DisableUndefinedErrorInUnityCloudBuilds.cs

---
+++ SDK/Editor/FoPC/DisableUndefinedErrorInUnityCloudBuilds.cs
@@ -0,0 +1,39 @@
+#if (UNITY_IOS && UNITY_EDITOR) && UNITY_CLOUD_BUILD
+using System;
+using System.IO;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.iOS;
+
+namespace MetaverseCloudEngine.Unity.Editors.FoPC
+{
+    public static class DisableUndefinedErrorInUnityCloudBuilds
+    {
+        [PostProcessBuild]
+        public static void OnPostProcessBuild(BuildTarget buildTarget, string pathToBuildProject)
+        {
+            if (buildTarget == BuildTarget.iOS)
+            {
+                Debug.Log("Adjusting LDFLAGS to ignore undefined symbols during build.");
+              
+                string pbxProjectPath = PBXProject.GetPBXProjectPath(pathToBuildProject);
+                PBXProject pbxProject = new PBXProject();
+                pbxProject.ReadFromFile(pbxProjectPath);
+    
+    #if UNITY_2019_3_OR_NEWER
+                string targetGuid = pbxProject.GetUnityFrameworkTargetGuid();
+    #else
+                string targetGuid = pbxProject.GetUnityMainTargetGuid();
+    #endif
+                pbxProject.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-Wl,-undefined,dynamic_lookup");
+                pbxProject.WriteToFile(pbxProjectPath);
+            }
+        }
+    }
+}
+#endif
